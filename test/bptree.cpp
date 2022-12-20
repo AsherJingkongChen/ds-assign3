bbaea4d2f5929cf167c34de1f8a251049338fc53
AsherChen <gpks556@gmail.com>
2022-12-21 03:40:21 +0800

    Asher Chen commited at 2022-12-21_03-40-21

--- test/bptree.cpp
+++ test/bptree.cpp
@@ -1,40 +1,66 @@
 #include "../third-party/SirLYC-BPTree/BPTree.h"
 #include "../src/cpp/header/test_header.hpp"
+#include <iostream>
+#include <fstream>
 
+using namespace std;
 using namespace ds;
-using namespace std::chrono_literals;
+using namespace chrono_literals;
 
 // ./<program> <size_in_2_power_of>
 //
 int main(int argc, char* argv[]) {
-  auto size_in_2_power_of(
+  size_t size_in_2_power_of(
     get_size_in_2_power_of_from_main(argc, argv)
   );
 
   // B+ Tree with order 4
   //
-  BPTree<int, int> db(4);
+  BPTree<int, int> st(4);
   urng<int> rng(1, 1 << 30);
-  timer<timeunit::msec> tim;
+  timer<timeunit::msec> clock;
+  csv data({
+    {"structure_name", "bplustree"},
+    {"size_in_2_power_of", to_string(size_in_2_power_of)},
+    {"insert_time_in_millisecond", {}},
+    {"search_time_in_millisecond", {}}
+  });
 
-  set_timeout(1h,
+  set_timeout(1s, // for test
   // to process
     [&]() {
-      for (auto t = from_2_power_of(size_in_2_power_of); t--;) {
-        db.put(rng(), rng());
+      clock.reset();
+      for (size_t t(from_2_power_of(size_in_2_power_of)); t--;) {
+        st.put(rng(), rng());
       }
+      clock.pause();
     },
 
   // on success
-    []() {
-
-    },
+    [&data, &clock]() { data[2] = to_string(clock.elapsed()); },
 
   // on failure
-    []() {
-
-    }
+    [&data, &clock]() { data[2] = ""; }
   );
+
+  set_timeout(1s, // for test
+  // to process
+    [&]() {
+      clock.reset();
+      for (size_t t(100000); t--;) {
+        st.get(rng());
+      }
+      clock.pause();
+    },
+
+  // on success
+    [&data, &clock]() { data[3] = to_string(clock.elapsed()); },
+
+  // on failure
+    [&data, &clock]() { data[3] = ""; }
+  );
+
+  cout << data.row_line() << flush;
 
   return 0;
 }
