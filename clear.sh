a2de6973ca6ccdd50ac65ea45e39579044865724
AsherChen <gpks556@gmail.com>
2022-12-25 03:43:01 +0800

    Asher Chen commited at 2022-12-25_03-43-01

--- clear.sh
+++ clear.sh
@@ -2,4 +2,5 @@
 # before running this script, please enter `chmod +x clear.sh`
 
 [ -d "bin" ] && rm -r bin && echo "rm -r bin";
+[ -d "log" ] && rm -r log && echo "rm -r log";
 [ -d "output" ] && rm -r output && echo "rm -r output";
